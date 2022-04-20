ae6a3103039199dd422c54a44923b93998a8952a
e.janbaz <D@nayar>
2022-04-20 12:39:56 +0400

    #5

--- contracts/Teams.sol
+++ contracts/Teams.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Teams {
 
+    address public owner;
     uint  teamId;
     uint[] teamIds;
     struct Team{
@@ -12,6 +13,15 @@ contract Teams {
     }
     mapping(uint => Team) teams;
     mapping(uint => uint[]) project_team;
+
+    constructor(){
+		owner = msg.sender;
+	}
+
+	modifier ownerOnly(){
+		require (msg.sender == owner , "You do not have permission for this action!");
+		_;
+	}
 
     function addNewTeam(uint _projectId, string memory _title) public {
         Team storage team = teams[++teamId];
@@ -28,4 +38,39 @@ contract Teams {
     function getTeamIdByProjectId(uint _projectId) public view returns(uint[] memory _teamIds){
         return project_team[_projectId];
     }
+
+    function findTeamIndexById(uint _teamId)internal view returns(uint) {
+        uint index = 0;
+        while (teamIds[index] != _teamId) {
+            index++;
+        }
+        return index;
+    }    
+
+    function findTeamIdByIndex(uint _teamIndex) public view returns(uint teamId){
+        return teamIds[_teamIndex];
+    }
+
+    function getTeamsCount() public view returns(uint count){
+        return teamIds.length;
+    }
+
+    function deleteTeam(uint _teamId) public ownerOnly{
+		delete teams[_teamId];
+		deleteTeamIdByShifting(_teamId);
+	}
+
+	function deleteTeamIdByShifting(uint _teamId) internal ownerOnly{
+		uint _index = findTeamIndexById(_teamId);
+		require (_index < teamIds.length , "Array index out of bound!");
+		for(uint i = _index; i < teamIds.length -1; i++){
+			teamIds[i] = teamIds[i + 1];
+		}
+		teamIds.pop();
+	}
+
+    function updateTeamTitle(uint _teamId,string memory _title) public ownerOnly{
+        teams[_teamId].title = _title;
+    }
+
 }
