fbac2dfc23a53679d3b6679a4fa7879b4cc5e869
e.janbaz <D@nayar>
2022-04-20 12:15:13 +0400

    #5

--- contracts/Teams.sol
+++ contracts/Teams.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Teams {
 
-    uint  teamId = 1;
+    uint  teamId;
     uint[] teamIds;
     struct Team{
         uint projectId;
@@ -14,11 +14,11 @@ contract Teams {
     mapping(uint => uint[]) project_team;
 
     function addNewTeam(uint _projectId, string memory _title) public {
-        Team storage team = teams[teamId];
+        Team storage team = teams[++teamId];
         team.title = _title;
         team.projectId = _projectId;
         team.createDate = block.timestamp;
-        project_team[_projectId].push(teamId++);
+        project_team[_projectId].push(teamId);
     }
 
     function getTeamById(uint _teamId) public view returns(Team memory team){
