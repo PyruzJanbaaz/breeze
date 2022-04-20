f1a9352b937ba969dab5448dc07d2b9caac0506d
e.janbaz <D@nayar>
2022-04-20 12:01:57 +0400

    #4

--- contracts/Projects.sol
+++ contracts/Projects.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Projects {
 
+    address public owner;
     uint  projectId;
     uint[] projectIds;
     struct Project{
@@ -11,14 +12,54 @@ contract Projects {
     }
     mapping(uint => Project) projects;
 
+	constructor(){
+		owner = msg.sender;
+	}
+
+	modifier ownerOnly(){
+		require (msg.sender == owner , "You do not have permission for this action!");
+		_;
+	}
+
     function addNewProject(string memory _title) public {
-        Project storage project = projects[projectId];
+        Project storage project = projects[++projectId];
         project.title = _title;
         project.createDate = block.timestamp;
-        projectIds.push(projectId++);
+        projectIds.push(projectId);
+    }
+
+    function findProjectIndexById(uint _projectId)internal view returns(uint) {
+        uint index = 0;
+        while (projectIds[index] != _projectId) {
+            index++;
+        }
+        return index;
+    }
+
+    function findProjectIdByIndex(uint _projectIndex) public view returns(uint projectId){
+        return projectIds[_projectIndex];
     }
 
     function getProjectById(uint _projectId) public view returns(Project memory project){
       return  projects[_projectId];
     }
+
+    function getProjectsCount() public view returns(uint count){
+        return projectIds.length;
+    }
+
+    function deleteProject(uint _projectId) public ownerOnly{
+		delete projects[_projectId];
+		deleteProjectIdByShifting(_projectId);
+	}
+
+	function deleteProjectIdByShifting(uint _projectId) internal ownerOnly{
+		uint _index = findProjectIndexById(_projectId);
+		require (_index < projectIds.length , "Array index out of bound!");
+		for(uint i = _index; i < projectIds.length -1; i++){
+			projectIds[i] = projectIds[i + 1];
+		}
+		projectIds.pop();
+	}
+
 }
---
+++ test/projects.test.js
@@ -0,0 +1,22 @@
+const Projects = artifacts.require("Projects");
+
+contract ('Projects', accounts =>{
+
+    let contractInstance = null;
+    before(async ()=>{
+        contractInstance = await Projects.deployed();
+    });
+
+    it('add new project', async()=>{
+        let projectsCount = parseInt(await contractInstance.getProjectsCount());
+        await contractInstance.addNewProject('Test1');
+		assert.equal(++projectsCount , parseInt(await contractInstance.getProjectsCount()));
+    });
+
+    it('delete a project', async ()=>{
+        let projectsCount = await contractInstance.getProjectsCount();
+        await contractInstance.deleteProject(await contractInstance.findProjectIdByIndex(projectsCount -1));
+        assert.equal(--projectsCount, parseInt(await contractInstance.getProjectsCount()));
+    });
+
+});
