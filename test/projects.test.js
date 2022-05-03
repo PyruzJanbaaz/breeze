8ae88e346f3eafa6fb7315c222303e39cc40c65e
pyruz <pyruz.janbaaz@gmail.com>
2022-05-03 12:14:16 +0400

    #13

--- test/projects.test.js
+++ test/projects.test.js
@@ -3,17 +3,43 @@ const Projects = artifacts.require("Projects");
 contract ('Projects', accounts =>{
 
     let contractInstance = null;
+    const TaskStatus = {
+        TODO: 0,
+        DOING: 1,
+        REVIEW: 2,
+        DONE: 3
+    };
     before(async ()=>{
         contractInstance = await Projects.deployed();
     });
 
-    it('add new project', async()=>{
+    it('add new project', async ()=> {
         let projectsCount = parseInt(await contractInstance.getProjectsCount());
         await contractInstance.addNewProject('Test1');
 		assert.equal(++projectsCount , parseInt(await contractInstance.getProjectsCount()));
     });
 
-    it('delete a project', async ()=>{
+    it('add new task on the project', async ()=> {
+        await contractInstance.addNewTask(1 , 'task title' , 'task description');
+        assert.equal((await contractInstance.getTaskById(1,1)).title, 'task title');
+    });
+
+    it('updte a task on the project', async ()=> {
+        await contractInstance.updateTask(1,1,'updated title', 'descriptoin');
+        assert.equal((await contractInstance.getTaskById(1,1)).title, 'updated title');
+    });
+
+    it('change a task status', async ()=> {
+        await contractInstance.changeTaskStatus(1,1,TaskStatus.DOING);
+        assert.equal((await contractInstance.getTaskById(1,1)).status, TaskStatus.DOING);
+    });
+
+    it('delete a task from the project', async ()=> {
+        await contractInstance.deleteTaskById(1,1);
+        assert.equal((await contractInstance.getTaskById(1,1)).title, '');
+    });
+
+    it('delete a project', async ()=> {
         let projectsCount = await contractInstance.getProjectsCount();
         await contractInstance.deleteProject(await contractInstance.getProjectIdByIndex(projectsCount -1));
         assert.equal(--projectsCount, parseInt(await contractInstance.getProjectsCount()));
--- test/users.test.js
+++ test/users.test.js
@@ -3,6 +3,12 @@ const Users = artifacts.require("Users");
 contract('Users',  accounts => {
 
 	let contractInstance = null;
+	const UserStatus = {
+		ACTIVE: 0, 
+		INACTIVE: 1, 
+		LOCKED: 2, 
+		PENDING:3 
+    };
 	before(async ()=>{
 		contractInstance = await Users.deployed();
 	});
@@ -16,7 +22,7 @@ contract('Users',  accounts => {
 	it('active user status', async ()=> {
 		await contractInstance.activeUserStatus(accounts[0]);
 		const userStatus = (await contractInstance.getUser(accounts[0])).status;		
-		assert.equal(userStatus , '0');
+		assert.equal(userStatus , UserStatus.ACTIVE);
 	});
 
 	it('delete user', async ()=>{
